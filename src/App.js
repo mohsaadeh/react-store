523053513d1f87e7aec18d084ba45fb86fc1542e
Mohammad Saadeh <moh.hsaadeh@gmail.com>
2023-06-12 23:50:54 +0300

    add react file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './style.css';
+import data from './data.json'
+import React,{useState} from 'react';
 function App() {
+  const [searchTerm,setSearchTerm]=useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+        <div className="lineTop">
+        <div className="line1">
+        </div>
+        <div className="line2">
+        </div>
     </div>
+
+
+
+ 
+    
+
+
+    <div className="logoHeader">
+      <a href='#'>
+      <img src="http://ahmad4d-003-site7.ftempurl.com/image/Asset-2-1.png" alt=""/>
+      </a>
+    </div>
+<div className='headernav'>
+<div className='SearchTop'>
+    <div className='SearchDiv'>
+      <input id='searchInput' type='text' className='textSearch' placeholder='Search here' onChange={(event)=> {
+        setSearchTerm(event.target.value);
+      } } />
+            <button class="btnSearch" type="submit">
+                    <i class="las la-search"></i>s
+                </button>
+    </div>
+    </div>
+</div>
+
+<div class="flex-grid-card">
+
+        {
+          data.filter((val) => {
+            if(searchTerm==""){
+              return val
+            }
+            else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+              return val;
+            }
+          })
+         .map((val)=>{
+          return (
+
+
+    
+
+
+            <div className='flex-grid-card-items' key={val.id}>
+
+              <img src={val.image} alt='Empty Image'></img>
+              <h3 className="flex-grid-card-title">{val.title}</h3>
+              <p className='flex-grid-card-price'>{val.price}</p>
+       
+              <div className="flex-grid-card-btn">
+               <button  type="submit">
+                   Select options
+                </button>
+         </div>   
+              </div>
+              
+          )
+         }) 
+        }
+ 
+
+    </div>
+    </>
+    
   );
 }
 
